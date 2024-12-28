48e023654e222a3a78b54958e9f0082090ed3288
Ash0Wash <bpower@sandiego.edu>
2024-12-28 12:28:30 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,23 @@ export default function Home() {
     <div className="container mx-auto px-4 pt-16">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <Link href="/work/concerts" className="group">
-          <div className="relative aspect-[4/3] overflow-hidden">
+            <div className="relative aspect-[4/3] overflow-hidden">
             <Image
-              src='/images/misc/-13.jpg'
+              src='/images/concerts/b3-08.jpg'
               alt="Concert photography"
               layout="fill"
-              className="object-cover transition-transform duration-500 group-hover:scale-105"
+              className="object-cover object-top transition-transform duration-500 group-hover:scale-105 transform"
             />
-          </div>
+            </div>
           <p className="mt-4 font-light">Concerts</p>
         </Link>
         <Link href="/work/portraits" className="group">
           <div className="relative aspect-[4/3] overflow-hidden">
             <Image
-              src="/images/misc/-02.jpg"
+              src="/images/portraits/-34.jpg"
               alt="Portrait photography"
               fill
-              className="object-cover transition-transform duration-500 group-hover:scale-105"
+              className="object-cover transition-transform duration-500 group-hover:scale-105 brightness-125"
             />
           </div>
           <p className="mt-4 font-light">Portraits</p>
